f316c07dcb726ea1bf879070e8cce4c51e8633b6
Amber Kimbrough (AJ) <94200525+AJKimbrough@users.noreply.github.com>
2023-07-11 15:41:32 -0500

    completes lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,26 @@
 def badge_maker(name):
-    return None
+    return f'Hello, my name is {name}.'
 
 def batch_badge_creator(names):
-    return None
+    new_list = []
+    for name in names:
+        new_list.append(f"Hello, my name is {name}.")
+    return new_list
 
 def assign_rooms(names):
-    return None
+    new_list = []
+    i = 1
+    for name in names:
+        new_list.append(f"Hello, {name}! You'll be assigned to room {i}!")
+        i+=1
+    return new_list
 
 def printer(names):
+    for name in batch_badge_creator(names):
+        print(name)
+    for name in assign_rooms(names):
+        print(name)
     return None
+
+
+printer((['Guido', 'Edsger', 'Ada', 'Charles', 'Alan', 'Grace', 'AJ']))
